6c73d1ef5712fa2f034e143e65d6282e5d19ce08
shock59 <42130427+shock59@users.noreply.github.com>
2026-03-01 15:36:19 +1100

    View any stat

---
+++ src/routes/[stat]/+page.server.ts
@@ -0,0 +1,78 @@
+import { readdir, readFile } from "node:fs/promises";
+
+const timeStats = [
+  "minecraft:sneak_time",
+  "minecraft:play_time",
+  "minecraft:time_since_death",
+  "minecraft:time_since_rest",
+  "minecraft:total_world_time",
+];
+const healthStats = [
+  "minecraft:damage_absorbed",
+  "minecraft:damage_blocked_by_shield",
+  "minecraft:damage_dealt",
+  "minecraft:damage_dealt_absorbed",
+  "minecraft:damage_dealt_resisted",
+  "minecraft:damage_resisted",
+  "minecraft:damage_taken",
+];
+const distanceStats = [
+  "minecraft:climb_one_cm",
+  "minecraft:crouch_one_cm",
+  "minecraft:fall_one_cm",
+  "minecraft:fly_one_cm",
+  "minecraft:sprint_one_cm",
+  "minecraft:swim_one_cm",
+  "minecraft:walk_one_cm",
+  "minecraft:walk_on_water_one_cm",
+  "minecraft:walk_under_water_one_cm",
+  "minecraft:boat_one_cm",
+  "minecraft:aviate_one_cm",
+  "minecraft:happy_ghast_one_cm",
+  "minecraft:horse_one_cm",
+  "minecraft:minecart_one_cm",
+  "minecraft:nautilus_one_cm",
+  "minecraft:pig_one_cm",
+  "minecraft:strider_one_cm",
+];
+
+export const load = async ({ params }) => {
+  const uuids = (await readdir("./input")).map((filename) =>
+    filename.substring(0, filename.length - 5),
+  );
+
+  const stats = await Promise.all(
+    uuids.map(async (uuid) => {
+      const response = await fetch(
+        `https://playerdb.co/api/player/minecraft/${uuid}`,
+      );
+      const json = await response.json();
+      const username = json.data.player.username as string;
+
+      const file = await readFile(`./input/${uuid}.json`, "utf-8");
+      const data = JSON.parse(file).stats as Record<
+        string,
+        Record<string, number>
+      >;
+
+      return { uuid, username, data };
+    }),
+  );
+
+  const statName = `minecraft:${params.stat}`;
+  return {
+    statName: statName,
+    stats: stats.toSorted(
+      (a, b) =>
+        (b.data["minecraft:custom"][statName] ?? 0) -
+        (a.data["minecraft:custom"][statName] ?? 0),
+    ),
+    type: timeStats.includes(statName)
+      ? "time"
+      : healthStats.includes(statName)
+        ? "health"
+        : distanceStats.includes(statName)
+          ? "distance"
+          : "number",
+  };
+};
---
+++ src/routes/[stat]/+page.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import language from "$lib/language";
+  import { onMount } from "svelte";
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
+
+  let format = $state((n: number) => n.toString());
+
+  onMount(() => {
+    if (data.type == "time") format = formatTime;
+    if (data.type == "health") format = formatHealth;
+    if (data.type == "distance") format = formatDistance;
+  });
+
+  function formatTime(n: number) {
+    return n >= 1_728_000
+      ? `${(n / 20 / 60 / 60 / 24).toFixed(2)} days`
+      : `${(n / 20 / 60 / 60).toFixed(2)} hours`;
+  }
+
+  function formatHealth(n: number) {
+    return n >= 20_000
+      ? `${(n / 10 / 2 / 1000).toFixed(1)}k hearts`
+      : `${(n / 10 / 2).toFixed(1)} hearts`;
+  }
+
+  function formatDistance(n: number) {
+    return n >= 100_000
+      ? `${(n / 100_000).toFixed(1)} km`
+      : `${(n / 100).toFixed(1)} meters`;
+  }
+</script>
+
+<h1>Minecraft Stats</h1>
+
+<h2>
+  {language[`stat.${data.statName.replace(":", ".")}` as keyof typeof language]}
+</h2>
+
+{#each data.stats as player}
+  <div class="player">
+    <img
+      src="https://minotar.net/helm/{player.uuid}/16"
+      alt={player.username}
+    />
+    {player.username}
+    <span class="right"
+      >{format(player.data["minecraft:custom"][data.statName] ?? 0)}</span
+    >
+  </div>
+{/each}
+
+<style>
+  h1 {
+    margin-top: 0;
+    font-size: xx-large;
+  }
+
+  div.player {
+    margin: 0 -8px;
+    padding: 8px;
+    display: flex;
+  }
+
+  div.player:nth-child(odd) {
+    background-color: #212121;
+  }
+
+  div.player img {
+    margin-right: 8px;
+    vertical-align: text-top;
+    image-rendering: pixelated;
+  }
+
+  .right {
+    margin-left: auto;
+  }
+</style>
