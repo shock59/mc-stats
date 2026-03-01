ac6f8fb19724c46682cf83d344934deee2b5976a
shock59 <42130427+shock59@users.noreply.github.com>
2026-03-01 16:49:13 +1100

    Add player heads to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,43 +7,23 @@
   let { data }: PageProps = $props();
 
   let query: string = $state("");
-  let stats: { id: string; name: string }[] = $state([]);
-  let searchedStats: { id: string; name: string }[] = $state([]);
+  let searchedStats: {
+    id: string;
+    name: string;
+    crownUuid: string | undefined;
+  }[] = $state([]);
 
   onMount(async () => {
-    stats = [
-      ...new Set(
-        data.stats.flatMap((player) =>
-          Object.keys(player.data["minecraft:custom"]),
-        ),
-      ),
-    ]
-      .map((stat) => ({
-        id: stat,
-        name: language[
-          `stat.${stat.replace(":", ".")}` as keyof typeof language
-        ],
-      }))
-      .toSorted((a, b) => {
-        if (a.name < b.name) {
-          return -1;
-        }
-        if (a.name > b.name) {
-          return 1;
-        }
-        return 0;
-      });
-
     search();
   });
 
   function search() {
     if (query.trim() == "") {
-      searchedStats = [...stats];
+      searchedStats = [...data.stats];
       return;
     }
 
-    const fuse = new Fuse(stats, {
+    const fuse = new Fuse(data.stats, {
       threshold: 0.3,
 
       keys: ["name"],
@@ -63,6 +43,15 @@
 {#each searchedStats as stat}
   <a href="/{stat.id.replace('minecraft:', '')}">
     <div class="stat">
+      {#if stat.crownUuid}
+        <img
+          src="https://minotar.net/helm/{stat.crownUuid}/16"
+          alt={stat.crownUuid}
+          class="head"
+        />
+      {:else}
+        <div class="head"></div>
+      {/if}
       {stat.name}
     </div></a
   >{/each}
@@ -88,6 +77,14 @@
     margin: 0 -8px;
     padding: 8px;
     display: flex;
+  }
+
+  .head {
+    width: 16px;
+    height: 16px;
+    margin-right: 8px;
+    vertical-align: text-top;
+    image-rendering: pixelated;
   }
 
   a:not(.hidden):nth-child(odd) div.stat {
