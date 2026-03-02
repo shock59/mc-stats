e5a5c2d2a8f024647d59e0f7e9e517907e48bfcc
shock59 <42130427+shock59@users.noreply.github.com>
2026-03-02 13:42:19 +1100

    Add a footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,20 +19,63 @@
       background: #121212;
       overflow-x: hidden;
     }
-
-    main {
-      max-width: 500px;
-      min-height: calc(100vh - 32px);
-      margin: 0 auto;
-      padding: 16px 22px;
-      color: #d0d0d0;
-      background: #1a1a1a;
-      font-family: "Minecraft Seven", sans-serif;
-      font-size: 16px;
-    }
   </style>
 </svelte:head>
 
-<main>
-  {@render children()}
-</main>
+<div id="container">
+  <main>
+    {@render children()}
+  </main>
+
+  <footer>
+    <span
+      >Made by <a href="https://samv.me/" target="_blank">Shock59</a> for
+      <a href="https://rayne.page/" target="_blank">Rayne</a></span
+    >
+  </footer>
+</div>
+
+<style>
+  #container {
+    max-width: 500px;
+    min-height: calc(100vh - 32px);
+    margin: 0 auto;
+    padding: 16px 22px;
+    display: grid;
+    grid-template-rows: 1fr auto;
+    color: #d0d0d0;
+    background: #1a1a1a;
+    font-family: "Minecraft Seven", sans-serif;
+    font-size: 16px;
+  }
+
+  footer {
+    margin-top: 16px;
+    padding-top: auto;
+    text-align: center;
+    font-size: 14px;
+  }
+
+  footer span {
+    opacity: 0.5;
+    transition: 200ms opacity;
+  }
+
+  footer span:hover {
+    opacity: inherit;
+  }
+
+  footer a {
+    text-decoration: none;
+    color: inherit;
+    transition: 200ms color;
+  }
+
+  footer span:hover a {
+    color: #5398ff;
+  }
+
+  footer a:hover {
+    text-decoration: underline;
+  }
+</style>
